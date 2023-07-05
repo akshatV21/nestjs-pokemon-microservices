23ef5c473dbfc5c98a5c5252af0dd33c9e911ce7
Akshat Vishwakarma <108259385+akshatV21@users.noreply.github.com>
2023-07-05 14:13:34 +0000

    added docker-compose support

--- apps/pokemon/src/main.ts
+++ apps/pokemon/src/main.ts
@@ -3,6 +3,6 @@ import { PokemonModule } from './pokemon.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PokemonModule);
-  await app.listen(3000);
+  await app.listen(8081);
 }
 bootstrap();
