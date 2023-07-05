83e6a331d984da7a2f2915b8e9cade1c1e6d6f47
Akshat Vishwakarma <108259385+akshatV21@users.noreply.github.com>
2023-07-05 14:27:12 +0000

    added env variable validation schema

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true, validationSchema: Joi.object({
+    PORT: Joi.number().required()
+  }) })],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/pokemon/src/pokemon.module.ts
+++ apps/pokemon/src/pokemon.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { PokemonController } from './pokemon.controller';
 import { PokemonService } from './pokemon.service';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true, validationSchema: Joi.object({
+    PORT: Joi.number().required()
+  }) })],
   controllers: [PokemonController],
   providers: [PokemonService],
 })
