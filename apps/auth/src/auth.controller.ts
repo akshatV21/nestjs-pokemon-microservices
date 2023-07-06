ef186cd4ce5f6b7bc298db6b03194f1ad63dc847
Akshat Vishwakarma <108259385+akshatV21@users.noreply.github.com>
2023-07-06 04:36:49 +0000

    Added interfaces in utils library

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { RegisterDto } from './dtos/register.dto'
+import { HttpSuccessResponse } from '@utils/utils'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async httpLoginUser(@Body() registerDto: RegisterDto) {
+  async httpLoginUser(@Body() registerDto: RegisterDto): Promise<HttpSuccessResponse> {
     const user = await this.authService.register(registerDto)
     return { success: true, message: 'User registered successfully.', data: { user } }
   }
---
+++ libs/utils/src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export interface HttpSuccessResponse {
+  success: boolean
+  message: string
+  data: Record<string, any>
+}
