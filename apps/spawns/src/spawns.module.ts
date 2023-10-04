b9ae12f065af89e7cbd3e7fdcec6042baa6e50a7
akshatV21 <a.vishwakarma2110@gmail.com>
2023-10-04 19:45:01 +0530

    Fixed joi import

--- apps/spawns/src/spawns.module.ts
+++ apps/spawns/src/spawns.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { SpawnsController } from './spawns.controller'
 import { SpawnsService } from './spawns.service'
 import { ConfigModule } from '@nestjs/config'
-import Joi from 'joi'
+import * as Joi from 'joi'
 import {
   BasePokemon,
   BasePokemonRepository,
